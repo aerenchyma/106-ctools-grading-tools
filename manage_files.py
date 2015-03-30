8be4b758ce7245bb67b1e40a38f8082f3401dd1c
jczetta <jczetta@gmail.com>
2015-03-30 18:17:01 -0400

    update for easier running

--- manage_files.py
+++ manage_files.py
@@ -8,6 +8,8 @@ if "necessary_files" not in [f for f in os.walk(".")][0][1]:
 elif "test106.py" not in [f for f in os.walk("necessary_files")][0][2]:
 	print "You probably need test106.py in the necessary files folder and it ain't there yet"
 
+#print os.getcwd()
+
 # Need to run this script from inside the bulk file, e.g. PS9
 # Need to save all files necessary for running people's programs
 filenames =[x for x in os.listdir("necessary_files")]
@@ -29,17 +31,19 @@ for d in dirs:
 			os.system("cp necessary_files/{} {}/Submission\ attachment\(s\)/".format(fp,dr)) # assuming ctools format
 	except:
 		print "I think your file structure is wrong. Take a look at it."
-# now run each and put test results/print output in a file with the name
 
+# now run each and put test results/print output in a file with the student name
 os.system("mkdir Test_Results") # create sub-folder in this main folder for all the test results
-for d in dirs: # now each has the right files in submission attachments
-	dr = d.replace(" ","\ ").replace("(","\(").replace(")","\)").replace(",","\,") # handled all chars
-	# run program 
-	#os.system("cd {}/Submission\ attachment\(s\)/".format(dr))
+for d in dirs: # each has the right files in submission attachments...
+	dr = d.replace(" ","\ ").replace("(","\(").replace(")","\)").replace(",","\,") # handled all chars for bash encoding
+	# now run program
 	try:
-		if "ps9.py" in [f for f in os.listdir("{}/Submission attachment(s)/".format(d))]:
-			print "Running {} problem set...".format(dr)
-			## the following works
+		curr_dir = str(os.getcwd()).lower()
+		# if sys.argv[1] in [f for f in os.listdir("{}/Submission attachment(s)/".format(d))]: # pass in filename expected as command line argument
+		if "{}.py".format(curr_dir) in [f.lower() for f in os.listdir("{}/Submission attachment(s)/".format(d))]: # if they have a file ps[whatever#folder] -- assume acting in eg. PS9 folder
+			print "Running {} problem set...".format(dr) # for clarity
+			# note: python or bash errors that occur will appear in console.
+			# to see these later, could pipe output of this master program to a file.
 			os.system("python {}/Submission\ attachment\(s\)/ps9.py > Test_Results/{}.txt".format(dr,dr)) # save output in Test_Results in a text file of the person's name
 		else:
 			print "Sorry, you'll need to rename {} pset to ps9.py...".format(dr)
